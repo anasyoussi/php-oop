b4688fd0243d4361f12e20cfc0df56c0cb0ab5d7
anasyoussi <18307320+anasyoussi@users.noreply.github.com>
2021-12-01 17:23:33 +0100

    Basics Of Namespaces (php 8.0.2)

--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,5 @@
 <?php 
-require_once '../Transaction.php'; 
+ 
+require_once '../PaymentGateway/Stripe/Transaction.php';  
 
-$transcation1 = new Transaction(100, 'Transaction 1') ; 
-$transcation2 = new Transaction(200, 'Transaction 2') ; 
-
-$transcation1->addTax(8)->applyDiscount(10)->getAmount();
-$transcation2->addTax(8)->applyDiscount(15)->getAmount();
-
-var_dump($transcation1->getAmount());
-echo "</br>";
-var_dump($transcation2->getAmount());
+var_dump(new Transaction(1, 'trans 1'));
